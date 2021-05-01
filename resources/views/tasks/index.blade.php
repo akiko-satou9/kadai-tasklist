63f18b9077d8b4d5f5120e8f483c8565daac6a98
akiko-satou9 <tekumaku.akkochan.k@gmail.com>
2021-05-01 08:19:49 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -19,7 +19,7 @@
                     {{-- 詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->status }}</td>
-                    td>{{ $task->content }}</td>
+                    <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
